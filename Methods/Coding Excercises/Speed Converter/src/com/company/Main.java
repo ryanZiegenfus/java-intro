93fcbe84e6a8d35ee27d65506efae8e6d5c9e807
Ryan <thewebcoderz@gmail.com>
2020-08-22 06:56:12 -0400

    completed Methods intro and Speed Conversion Challenge

---
+++ Methods/Coding Excercises/Speed Converter/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour >= 0) {
+            long mph = Math.round(kilometersPerHour / 1.609);
+            return mph;
+        }
+        return -1;
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if(kilometersPerHour >= 0) {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
