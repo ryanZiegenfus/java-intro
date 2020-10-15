38aa92e65045a0feb02f84e9907ff49c3f1fc58c
Ryan <thewebcoderz@gmail.com>
2020-10-14 22:12:00 -0400

    finished Inheritance Module 1

---
+++ OOP/Inheritance/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal", 1, 1, 5, 10);
+
+	    Dog dog = new Dog("fluffy", 6, 12, 2, 4, 1, 30, "fluff");
+	    dog.eat();
+    }
+}
